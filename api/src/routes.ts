f9ee93520d1b7786734a49750c4ec396d4c11631
leandrosouzaa <leandror.dev@gmail.com>
2020-10-13 20:10:30 -0300

    add image uplaod

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { getRepository } from 'typeorm';
+
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 import Orphanage from './models/Orphanage';
 
+
 const routes = Router();
+
+const upload = multer(uploadConfig)
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages',upload.array('images'), OrphanagesController.create);
 
 
-export default routes
+export default routes;
