00364adc6af71d59b9f9b5688408542275937967
leandrosouzaa <leandror.dev@gmail.com>
2020-10-13 20:43:34 -0300

    static files

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,14 +1,14 @@
 import express, { response } from 'express';
+import path from 'path';
+
 import './database/connection';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.use(routes)
-
-
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname,'..', 'uploads')));
 
 app.listen(3333, () => {
    console.log("👷 Api running on port 33333");
