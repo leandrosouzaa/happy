a962dbcbc531c850f042afbe5a3d80215a0f7efb
leandrosouzaa <leandror.dev@gmail.com>
2020-10-15 21:57:47 -0300

    init OrphanageDetails screen

---
+++ mobile/src/screens/OrphanageDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const OrphanageDetails: React.FC = () => {
+   return (
+      <View>
+         <Text>Tela De detalhes</Text>
+      </View>
+   )
+}
+
+export default OrphanageDetails;
